5f4b14dbb7eeeb2575a9fcb0d7b84f2b72ed72f7
moogiecodes <mchoichoi@gmail.com>
2020-01-23 18:22:19 -0800

    Add flash, session, and begin POST route

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 from surveys import satisfaction_survey, personality_quiz
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
 form_responses = []
-# go off len
 
 
 @app.route("/")
@@ -15,12 +16,26 @@ def home():
         "index.html",
         survey_title=title,
         survey_instructions=instructions
-        # satisfaction_survey=satisfaction_survey
     )
+
+
+@app.route('/begin', methods=['POST'])
+def begin():
+    session['responses'] = []
+    return redirect('/questions/0')
 
 
 @app.route("/questions/<int:question_num>")
 def show_question(question_num):
+    form_responses = session.get('responses')
+
+    if len(form_responses) != question_num:
+        flash('You are accessing an invalid question!')
+        return redirect(f"/questions/{len(form_responses)}")
+
+    if len(form_responses) == len(satisfaction_survey.questions):
+        return redirect('/thanks')
+
     question = satisfaction_survey.questions[question_num].question
     choices = satisfaction_survey.questions[question_num].choices
 
@@ -28,14 +43,25 @@ def show_question(question_num):
                            question=question,
                            choices=choices,
                            )
-    # question_num=question_num
 
 
 @app.route('/answer', methods=['POST'])
 def handle_answer():
     answer = request.form.get('choice')
-    form_responses.append(answer)
+    # form_responses.append(answer)
 
-    # REDIRECT to question/num
+    form_responses = session['responses']
+    form_responses.append(answer)
+    session['responses'] = form_responses
+
+    if len(form_responses) == len(satisfaction_survey.questions):
+        return redirect('/thanks')
+
     return redirect(f"/questions/{len(form_responses)}")
-    # question=question, choices=choices
+
+
+@app.route("/thanks")
+def thanks():
+
+    return render_template(
+        "thanks.html")
